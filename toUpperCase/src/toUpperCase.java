93db441fb93d05f0e6c7f0d9a04f79f31dd096b0
NGeorgiev <NGeorgiev@spartaglobal.com>
2019-10-14 13:00:39 +0100

    ToUpperCase

---
+++ toUpperCase/src/toUpperCase.java
@@ -0,0 +1,10 @@
+public class toUpperCase {
+    public static void main(String[] args) {
+        String toUpper = "hello";
+        System.out.println(toUpper);
+
+        System.out.println(toUpper.toUpperCase());
+
+    }
+
+}
